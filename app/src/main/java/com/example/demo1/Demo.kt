9caea681ae30a6da7b2d64bd7092415b5adc985e
dobafisoye <119076518+dobafisoye@users.noreply.github.com>
2024-08-26 09:34:00 -0500

    Initial Commit

---
+++ app/src/main/java/com/example/demo1/Demo.kt
@@ -0,0 +1,35 @@
+package com.example.demo1
+
+fun main(){
+
+    // single line comment
+
+    /*
+    multi line comment
+     */
+
+    println("Hello World")
+
+    // variable declaration
+    val num = 10 // read only variable
+    var num2 = 23 // writeable variable
+
+    /*
+    DATA TYPES
+    Primitives
+        Integer, Double, Boolean
+
+    Non-primitive
+        String, Array Classes
+     */
+
+    val multiLineString = """
+        This is line 1
+        This is line 2
+        This is line 3
+    """
+
+    println(multiLineString)
+
+
+}
